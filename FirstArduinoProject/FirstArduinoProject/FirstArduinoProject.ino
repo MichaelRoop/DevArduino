6c1f67d992c487f8745024ac5bcf245c0580b8bb
michael@roop.ca <michael@roop.ca>
2019-12-11 18:16:55 -0400

    Read the incoming string from Bluetooth looking for \r\n to get in one string

--- FirstArduinoProject/FirstArduinoProject/FirstArduinoProject.ino
+++ FirstArduinoProject/FirstArduinoProject/FirstArduinoProject.ino
@@ -14,8 +14,8 @@ bool hasInput = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	//SetupDataComs();
-	SetupAtComs();
+	SetupDataComs();
+	//SetupAtComs();
 
 	//SetupCommunications();
 	//pinMode(13, OUTPUT); 
@@ -25,18 +25,22 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 	//SimpleSerialOutput();
-	//ListenToBT();
-	SendBT_AT();
+	ListenToBT();
+	//SendBT_AT();
 }
 
 
 void SetupDataComs() {
-	SetupCommunications(115200, 9600);
+	//SetupCommunications(115200, 9600);
+	SetupCommunications(9600, 38400);
 }
 
 void SetupAtComs() {
 	// Need 38400 for BT and 9600 for serial to send AT commands
-	// strange because BT AT cmd shows BT baud is 9600
+	// strange because BT AT cmd shows BT baud is 9600. 
+	// If I set BT serial higher via an AT command, 
+	// it still only works if I set the software serial for BT at 38400
+	// Although we can set the debug serial higher 9600 is adequate
 	SetupCommunications(9600, 38400);
 }
 
@@ -66,9 +70,19 @@ void ListenToBT(){
 	if(btSerial.available() > 0) {  
 		hasInput = true;
 		// Just bounce it to the serial output for debugging 
-		Serial.write(btSerial.read());
+		//Serial.write(btSerial.read());
 		// In the real world we would loop through all the characters and
 		// assemble the message. Then act on message
+
+		// Terminator is sequence of \r\n for data if sent from app connecting via Bluetooth
+		//char val = (char)btSerial.read();
+		//if (val == '\r') {
+		//	Serial.write("\\r");
+		//}
+		//else if (val == '\n') {
+		//	Serial.write("\\n");
+		//}
+		//Serial.write(val);
 	}
 	else {
 		if (hasInput){
@@ -100,13 +114,39 @@ void ListenToBT(){
  * AT+ROLE		+ROLE:0 (1=master, 0=slave)
  * AT+PSWD		+PSWD:1234 (pairing PIN)
  *
+ * incoming responses from Bluetooth are ended with \r\n
 */
+#define IN_BUFF_SIZE 100
+char buff[IN_BUFF_SIZE];
+unsigned char inIndex = 0;
 void SendBT_AT() {
+
 	// Stream any input from bluetooth to the debug serial stream
 	// each entry 1 character (int). Each response finished with \r\n
 	if (btSerial.available() > 0) {
-		Serial.write(btSerial.read());
+		buff[inIndex] = (char)btSerial.read();
+		if (buff[inIndex] == '\n') {
+			Serial.write(buff);
+			// In a normal program we could see what msg came in and act on it
+			// Not sure what comes in when in data mode
+			memset(buff, 0, IN_BUFF_SIZE);
+			inIndex = 0;
+		}
+		else {
+			inIndex++;
+			// Wipe out buffer if too long
+			if (inIndex >= IN_BUFF_SIZE) {
+				inIndex = 0;
+				Serial.println("Corrupt BT input. Purge buffer");
+			}
+		}
 	}
+
+	// This is the simple version which just pushes each character to debug serial
+	//if (btSerial.available() > 0) {
+	//	Serial.write(btSerial.read());
+	//}
+
 
 	// Get AT cmd from Serial monitor incoming stream to send to Bluetooth
 	if (Serial.available() > 0) {
