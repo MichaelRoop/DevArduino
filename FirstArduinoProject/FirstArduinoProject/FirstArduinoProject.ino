d5c064032ad4e5f38da216c9b36e83eb97f79fe4
michael@roop.ca <michael@roop.ca>
2019-12-11 15:21:21 -0400

    Two separate routines, one for AT command mode and the other for data mode

--- FirstArduinoProject/FirstArduinoProject/FirstArduinoProject.ino
+++ FirstArduinoProject/FirstArduinoProject/FirstArduinoProject.ino
@@ -14,7 +14,10 @@ bool hasInput = false;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-	SetupCommunications();
+	//SetupDataComs();
+	SetupAtComs();
+
+	//SetupCommunications();
 	//pinMode(13, OUTPUT); 
 }
 
@@ -22,20 +25,31 @@ void setup() {
 // the loop function runs over and over again until power down or reset
 void loop() {
 	//SimpleSerialOutput();
-	ListenToBT();
-
-	// To send a message through BT
-	//btSerial.write("From board\n"); 
-	//Serial.println("AT+NAME?/r/n");
+	//ListenToBT();
+	SendBT_AT();
 }
 
 
-void SetupCommunications(){
-	Serial.begin(9600);
-	btSerial.begin(9600);
+void SetupDataComs() {
+	SetupCommunications(115200, 9600);
+}
+
+void SetupAtComs() {
+	// Need 38400 for BT and 9600 for serial to send AT commands
+	// strange because BT AT cmd shows BT baud is 9600
+	SetupCommunications(9600, 38400);
+}
+
+
+void SetupCommunications(long dbg, long bt){
+	btSerial.begin(bt);
+	Serial.begin(dbg);
+
 	while (!Serial) {
 		; // wait for serial port to connect. Needed for Native USB only
 	}
+	Serial.println("Debug serial active");
+	// example had pin 9 set low, then high but does not seem necessary
 }
 
 
@@ -71,5 +85,31 @@ void ListenToBT(){
 		i++;
 		delay(100);
 	}
+}
 
+/**
+ * Receive AT commands from serial monitor and pass them to Bluetooth module.
+ * Set the serial monitor to add LF and CRL to outgoing
+ * The shield must already be switched via toggle to CMD mode. Cannot be done
+ * programatically.  Here is the configuration info from AT commands
+ *
+ * AT 			+NAME:itead
+ * AT+VERSION 	+VERSION:hc01.comV2.1
+ * AT+ADDR		+ADDR:2016:4:76101
+ * AT+UART		+UART:9600,0,0
+ * AT+ROLE		+ROLE:0 (1=master, 0=slave)
+ * AT+PSWD		+PSWD:1234 (pairing PIN)
+ *
+*/
+void SendBT_AT() {
+	// Stream any input from bluetooth to the debug serial stream
+	// each entry 1 character (int). Each response finished with \r\n
+	if (btSerial.available() > 0) {
+		Serial.write(btSerial.read());
+	}
+
+	// Get AT cmd from Serial monitor incoming stream to send to Bluetooth
+	if (Serial.available() > 0) {
+		btSerial.write(Serial.read());
+	}
 }
