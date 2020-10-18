5701ba643da93f505e78e67bf1f6610ca779e412
michael@roop.ca <michael@roop.ca>
2020-10-18 01:27:07 -0300

    Up the baud on serial - no return data for now

--- FirstArduinoProject/ArduinoWifi/ArduinoWifi.ino
+++ FirstArduinoProject/ArduinoWifi/ArduinoWifi.ino
@@ -16,7 +16,7 @@ int led = LED_BUILTIN;
 WiFiServer server(80); // Port 80 http
 
 void setup() {
-	Serial.begin(9600);
+	Serial.begin(57600);
 	while(!Serial){}
 	Serial.println("Serial started");
 
@@ -93,12 +93,67 @@ void loop() {
 			Serial.println("Device disconnected from AP");
 		}
 	}
-
-
-
-
-
+	ListenForClient();
 }
+
+
+void ListenForClient() {
+	//https://www.arduino.cc/en/Reference/WiFiNINABeginAP
+	WiFiClient client = server.available();
+	if (client) {                     
+		Serial.println("Got a client connected new client"); 
+		String currentLine = ""; 
+
+		// loop while the client's connected
+		while (client.connected()) {
+			if (client.available()) {
+				// Read a byte then print it out to serial monitor
+				char c = client.read();
+				Serial.write(c);
+				// This will bounce it back
+			//	client.print(c);
+
+
+				//// This example is for HTTP. I Just want to get bytes
+				//if (c == '\n') {
+				//	// if the current line is blank, you got two newline characters in a row.
+				//	// that's the end of the client HTTP request, so send a response:
+				//	if (currentLine.length() == 0) {
+				//		// HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+				//		// and a content-type so the client knows what's coming, then a blank line:
+				//		client.println("HTTP/1.1 200 OK");
+				//		client.println("Content-type:text/html");
+				//		client.println();
+				//		// the content of the HTTP response follows the header:
+				//		client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
+				//		client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
+				//		// The HTTP response ends with another blank line:
+				//		client.println();
+				//		// break out of the while loop:
+				//		break;
+				//	}
+				//	else {      // if you got a newline, then clear currentLine:
+				//		currentLine = "";
+				//	}
+				//}
+				//else if (c != '\r') {    // if you got anything else but a carriage return character,
+				//	currentLine += c;      // add it to the end of the currentLine
+				//}
+				//// Check to see if the client request was "GET /H" or "GET /L":
+				//if (currentLine.endsWith("GET /H")) {
+				//	digitalWrite(led, HIGH);               // GET /H turns the LED on
+				//}
+				//if (currentLine.endsWith("GET /L")) {
+				//	digitalWrite(led, LOW);                // GET /L turns the LED off
+				//}
+			}
+		} // While connected - socket?
+		// close the connection:
+		client.stop();
+		Serial.println("client disconnected");
+	}
+}
+
 
 
 
