34f52ba0ea45df838d37895017de46d8e29697a0
michael@roop.ca <michael@roop.ca>
2021-10-09 18:05:45 -0300

    Change of build stuff

---
+++ FirstArduinoProject/HardwareDebugTest/HardwareDebugTest.ino
@@ -0,0 +1,22 @@
+/*
+ Name:		HardwareDebugTest.ino
+ Created:	10/9/2021 5:06:02 PM
+ Author:	Michael
+*/
+
+
+//#include <avr8-stub.h>
+
+int x = 0;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	//debug_init();
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  //breakpoint();
+
+  x++;
+}
